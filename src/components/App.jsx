faa860f53bf3d541c94509715c285cb0de708527
Amalynn <amandine.ebner@gmail.com>
2024-07-15 14:48:15 +0200

    Router setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom" ;
+import Home from "../pages/Home.jsx";
+import Credits from "../pages/Credits.jsx";
+import Error404 from "../pages/Error404.jsx";
 
+export default function App() {
   return (
-    <>
-      <h1>Hello World !</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mentions-legales-credits" element={<Credits />} />
+        <Route path="/error-page-not-found" element={<Error404/>} />
+        <Route path="*" element={<Navigate to="/error-page-not-found" replace />} />
+      </Routes>
+    </Router>   
   )
 }
-
-export default App
