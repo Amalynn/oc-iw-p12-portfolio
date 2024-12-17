c57f1e750f732d2f7ac4dbef4d6623d166c4147f
Amalynn <amandine.ebner@gmail.com>
2024-12-17 10:43:16 +0100

    Projects cards and filter buttons implemented

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,9 +1,14 @@
 import PropTypes from "prop-types";
 
-export default function FilterButton({ children }) {
-    return <button className="filter-button">{children}</button>;
+export default function FilterButton({ children, classNames, ...rest }) {
+    return (
+        <button className={`filter-button ${classNames}`} {...rest}>
+            {children}
+        </button>
+    );
 }
 
 FilterButton.propTypes = {
     children: PropTypes.string.isRequired,
+    classNames: PropTypes.string.isRequired,
 };
--- src/containers/ProjectsList.jsx
+++ src/containers/ProjectsList.jsx
@@ -1,69 +1,70 @@
 import FilterButton from "../components/FilterButton";
 import ProjectCard from "../components/ProjectCard";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DataContext } from "../contexts/DataContext";
 
 export default function ProjectsList() {
     const data = useContext(DataContext);
-    console.log(data);
-    // const categoriesProjectList = new Set(
-    //     data?.projects.map((project) => project.categories)
-    // ).add("Tous");
-    // console.log(categoriesProjectList);
-    // return (
-    //     <div>
-    //         {Array.from(categoriesProjectList).map((category) => {
-    //             <FilterButton key={category}>{category}</FilterButton>;
-    //         })}
-    //     </div>
-    // );
+    const [category, setCategory] = useState();
+    const [activeCategory, setActiveCategory] = useState("Tous");
+
+    const projectsVisible = data?.projects.filter(
+        (project) => project.isVisible === true
+    );
+
+    const projectCategoriesList = new Set(
+        projectsVisible.map((project) => project.category)
+    );
+
+    const filteredProjects = projectsVisible.filter((project) =>
+        !category || category === "Tous"
+            ? projectsVisible
+            : project.category === category
+    );
+
+    const handleClickChangeCategory = (event) => {
+        let category = event.target.innerText;
+        setCategory(category);
+        setActiveCategory(category);
+    };
+
     return (
         <>
             <div className="filter-button-group">
-                <FilterButton>Tous</FilterButton>
-                <FilterButton>React</FilterButton>
-                <FilterButton>Gestion de projet</FilterButton>
-                <FilterButton>PHP</FilterButton>
-                <FilterButton>SEO</FilterButton>
-                <FilterButton>Débugage</FilterButton>
+                <FilterButton
+                    onClick={handleClickChangeCategory}
+                    classNames={
+                        activeCategory === "Tous" ? "filter-button--active" : ""
+                    }
+                >
+                    Tous
+                </FilterButton>
+                {projectCategoriesList &&
+                    Array.from(projectCategoriesList).map((category) => (
+                        <FilterButton
+                            key={category}
+                            onClick={handleClickChangeCategory}
+                            classNames={
+                                activeCategory === category
+                                    ? "filter-button--active"
+                                    : ""
+                            }
+                        >
+                            {category}
+                        </FilterButton>
+                    ))}
             </div>
 
             <div className="project-card-list">
-                <ProjectCard
-                    title="Nina Carducci"
-                    srcImage="/images/projects/ninacarducci/ninacarducci-cover.jpg"
-                    label="SEO"
-                />
-                <ProjectCard
-                    title="CepAtlas"
-                    srcImage="/images/projects/cepatlas/cepatlas-cover.jpg"
-                    label="PHP"
-                />
-                <ProjectCard
-                    title="724events"
-                    srcImage="/images/projects/724events/724events-cover.jpg"
-                    label="Débugage"
-                />
-                <ProjectCard
-                    title="Argent Bank"
-                    srcImage="/images/projects/argentbank/argentbank-cover.jpg"
-                    label="React"
-                />
-                <ProjectCard
-                    title="Kasa"
-                    srcImage="/images/projects/kasa/kasa-cover.jpg"
-                    label="React"
-                />
-                <ProjectCard
-                    title="Menu Maker"
-                    srcImage="/images/projects/menumaker/menumaker-cover.jpg"
-                    label="Gestion de projet"
-                />
-                <ProjectCard
-                    title="Portfolio"
-                    srcImage="/images/projects/portfolio/portfolio-cover.jpg"
-                    label="React"
-                />
+                {filteredProjects &&
+                    filteredProjects.map((project) => (
+                        <ProjectCard
+                            key={project.id}
+                            title={project.name}
+                            srcImage={project.cover}
+                            label={project.category}
+                        />
+                    ))}
             </div>
         </>
     );
