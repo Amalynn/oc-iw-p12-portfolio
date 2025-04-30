416b4cef10204e13aff2a0079098d3e3a380fb30
Amalynn <amandine.ebner@gmail.com>
2025-04-30 16:14:37 +0200

    Updated CV in PDF format

--- src/components/buttons/DownloadCvButton.jsx
+++ src/components/buttons/DownloadCvButton.jsx
@@ -1,9 +1,12 @@
 import Button from "./Button";
 
-export default function DownloadCvButton(){
+export default function DownloadCvButton() {
     return (
-        <Button href="/pdf/CV-Amandine_Ebner_test.pdf" classesNames="button button-download-cv">
+        <Button
+            href="/pdf/CV-Amandine_EBNER-Developpeuse_Frontend_ReactJS.pdf"
+            classesNames="button button-download-cv"
+        >
             Télécharger mon CV
         </Button>
-    )
+    );
 }
