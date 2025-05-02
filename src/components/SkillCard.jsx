6ef07bcdafe42f1b102979e0f5e7b235436de5b3
Amalynn <amandine.ebner@gmail.com>
2025-05-02 15:02:37 +0200

    Texts in the Skills and About sections updated and font size modifying.

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,21 +1,21 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-export default function SkillCard({icon, name, description}) {
+export default function SkillCard({ icon, name, description }) {
     return (
         <article className="skill-card">
             <div className="skill-card-header">
                 <i className={icon}></i>
             </div>
             <div className="skill-card-body">
-                <h4>{name}</h4>
+                <h3>{name}</h3>
                 <p>{description}</p>
             </div>
         </article>
-    )
+    );
 }
 
 SkillCard.propTypes = {
-    icon: PropTypes.string.isRequired,    
+    icon: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired
-}
+    description: PropTypes.string.isRequired,
+};
--- src/containers/AproposContent.jsx
+++ src/containers/AproposContent.jsx
@@ -1,23 +1,54 @@
 import ProfileSmall from "../assets/images/profile/profile-200w.webp";
 import ProfileLarge from "../assets/images/profile/profile-400w.webp";
 
-
 export default function AproposContent() {
     return (
         <div className="apropos-content">
             <div className="apropos-content-profile">
                 <img
-                    srcSet= {`${ProfileSmall} 200w}, ${ProfileLarge} 400w `}
-                    sizes= "(min-width: 992px) 400px, 200px "
-                    src={ProfileSmall} 
-                    alt="My profile avatar"  
+                    srcSet={`${ProfileSmall} 200w}, ${ProfileLarge} 400w `}
+                    sizes="(min-width: 992px) 400px, 200px "
+                    src={ProfileSmall}
+                    alt="My profile avatar"
                 />
             </div>
             <div className="apropos-content-description">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt, tempore. Accusamus explicabo sit ut qui aliquam atque voluptate veniam distinctio laudantium earum recusandae delectus voluptatibus consequuntur magnam veritatis sunt, architecto sapiente impedit dolore incidunt iusto dolorum fugiat soluta. Sequi id necessitatibus, repellat magni quas molestias dolores veniam inventore reiciendis fuga officiis iure possimus voluptatum cum blanditiis quibusdam libero est qui tenetur consectetur nam. Repellendus eos, laboriosam quos eveniet beatae labore quia aut aliquam quae laborum ducimus unde, tempore accusantium blanditiis?</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, rerum. Atque asperiores quibusdam ut at ex excepturi, aspernatur sit neque ducimus omnis. Doloremque mollitia, aspernatur unde iusto magnam libero id consequatur enim vitae reprehenderit saepe consectetur perferendis nesciunt veritatis corporis repellat dolor commodi natus necessitatibus facere amet explicabo adipisci nam.</p>
+                <p>
+                    Passionnée par l&apos;histoire minière de ma région, je me
+                    suis tournée vers la géologie avec un BTS de Géologie
+                    Appliquée (Nancy) puis un master en &quot;Exploration et
+                    Géomatique des Ressources Minérales (Université
+                    d&apos;Orléans). J&apos;ai travaillé quelques années en tant
+                    que technicienne géologue de terrain: suivre les campagnes
+                    de sondages, réaliser des campagnes de prospection
+                    géophysique, préparer mes missions et mon matériel, seule,
+                    en binôme ou en équipe étaient mon quotidien.
+                </p>
+                <p>
+                    Je me suis orientée ensuite vers des projets de R&D toujours
+                    en géosciences mais cette fois-ci plutôt axés dans les
+                    domaines de la télédétection et géomatique. Entrée dans ce
+                    monde fut captivant et stimulant. Passée du terrain à la
+                    recherche m&apos;a permis d&apos;explorer de nouvelles
+                    thématiques et méthodes de travail.
+                </p>
+                <p>
+                    C&apos;est ma dernière expérience avec le développement du
+                    programme de recherche CepAtlas qui m&apos;a amené à cotoyer
+                    le domaine du web. Concevoir, imaginer, retranscrire une
+                    idée en code, trouver des solutions pour mener à bien le
+                    projet confié, travailler avec une équipe pluridisciplinaire
+                    sont autant de raisons qui m&apos;ont fait aimer travailler
+                    sur ce type de projet.
+                </p>
+                <p>
+                    Aujourd&apos;hui, j&apos;ai consolidé mes compétences en
+                    développement web en suivant la formation &quot;Développeur
+                    web/Intégrateur web&quot; d&apos;OpenClassrooms.
+                    L&apos;objectif : &quot;Participer à des projets innovants
+                    alliant développement web, géosciences et géomatique.&quot;
+                </p>
             </div>
-
         </div>
-    )
+    );
 }
